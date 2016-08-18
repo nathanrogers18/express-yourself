ca5a194341bba62fcadf723eebc40ff9dd4fc5d1
Nathan Rogers <nrogers@nrogers-macbookpro.roam.corp.google.com>
2016-08-18 09:26:50 -0400

    finished extractor

--- textminer/extractor.py
+++ textminer/extractor.py
@@ -1,6 +1,9 @@
+import re
+
+
 def phone_numbers(text):
-    pass
+    return re.findall(r"\(*\d{3}\)*[\s.-]*\d{3}[\s.-]*\d{4}", text)
 
 
 def emails(text):
-    pass
+    return re.findall(r"\w*\.*\w+@[a-zA-Z]+\.[a-zA-Z]{2,3}", text)
--- textminer/separator.py
+++ textminer/separator.py
@@ -1,23 +1,36 @@
+import re
+s = "bob sue jon richard harry"
+r = re.compile('(?P<name>[a-z]+)\s+(?P<name2>[a-z]+)')
+[m.groupdict() for m in r.finditer(s)]
+[{'name2': 'sue', 'name': 'bob'}, {'name2': 'richard', 'name': 'jon'}]
+
+
 def words(input):
-    pass
+    rgx = re.compile("(?P<name>[a-z]+)\s+(?P<name2>[a-z]+)")
+    return [m.groupdict() for rgx in rgx.finditer(input)]
 
 
 def phone_number(input):
-    pass
+    rgx = re.compile("(?P<name>[a-z]+)\s+(?P<name2>[a-z]+)")
+    return [m.groupdict() for rgx in rgx.finditer(input)]
 
 
 def money(input):
-    pass
+    rgx = re.compile("(?P<name>[a-z]+)\s+(?P<name2>[a-z]+)")
+    return [m.groupdict() for rgx in rgx.finditer(input)]
 
 
 def zipcode(input):
-    pass
+    rgx = re.compile("(?P<name>[a-z]+)\s+(?P<name2>[a-z]+)")
+    return [m.groupdict() for rgx in rgx.finditer(input)]
 
 
 def date(input):
-    pass
+    rgx = re.compile("(?P<name>[a-z]+)\s+(?P<name2>[a-z]+)")
+    return [m.groupdict() for rgx in rgx.finditer(input)]
 
 
 # ADVANCED MODE
 def address(input):
-    pass
+    rgx = re.compile("(?P<name>[a-z]+)\s+(?P<name2>[a-z]+)")
+    return [m.groupdict() for rgx in rgx.finditer(input)]
